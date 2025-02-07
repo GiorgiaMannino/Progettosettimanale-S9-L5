2d4d07d3e55b9ac844cca6a48c039dc01369f69a
GiorgiaMannino <giorgimannino@hotmail.com>
2025-02-07 13:26:57 +0100

    fetch componente HarryPotter

--- src/components/HarryPotterGallery.jsx
+++ src/components/HarryPotterGallery.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 class HarryPotterGallery extends Component {
   state = {
@@ -17,16 +18,54 @@ class HarryPotterGallery extends Component {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        if (data.Response === "True") {
+          this.setState({
+            movies: data.Search,
+            loading: false,
+          });
+        } else {
+          this.setState({
+            error: data.Error,
+            loading: false,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Errore nel fetch:", error);
+        this.setState({
+          error: "Errore nel recupero dei film",
+          loading: false,
+        });
       });
   };
 
   render() {
+    const { movies, loading, error } = this.state;
+
+    if (loading) {
+      return <div>Caricamento in corso...</div>;
+    }
+
+    if (error) {
+      return <div>Errore: {error}</div>;
+    }
+
+    const firstSixMovies = [...movies];
+
     return (
-      <div>
-        <h2>Trending Now</h2>
-      </div>
+      <Container>
+        <Row>
+          <h5 className="bg-dark text-white p-3">Trending Now</h5>
+          {firstSixMovies.map((movie) => (
+            <Col key={movie.imdbID}>
+              <div className="mb-5">
+                <img src={movie.Poster} alt={movie.Title} />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     );
   }
 }
-
 export default HarryPotterGallery;
